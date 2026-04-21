d7bf21ed1c45ef317324119d29cf32157368811e
Muzamil Hussain <muzamilhussain369@gmail.com>
2026-04-21 16:56:08 +0500

    add frontend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://shinvo.duckdns.org/api',
     headers: {
         'Content-Type': 'application/json'
     }
